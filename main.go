23da07197871aaf67a4f45ed0a98932642f183ff
Yuttasak(Yo)Pannawat <yuttasak.pan.true@gmail.com>
2023-07-13 22:37:39 +0700

    refactor(validation-password)Custom validation password

--- main.go
+++ main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/yuttasakcom/go-errors-simple/validation"
 )
 
-type Unwrappable struct {
-	msg     string
-	wrapped error
-}
-
-func (u Unwrappable) Error() string {
-	return u.msg
-}
-
-func (u Unwrappable) Unwrap() error {
-	return u.wrapped
-}
-
 func main() {
 	if err := validation.PasswordValidation("#"); err != nil {
-		fmt.Printf("%+v", *err.(*validation.PasswordError))
+		fmt.Printf("%v\n", err)
+		fmt.Println("------------")
+		if errors.Is(err, validation.ErrInvalidLength) {
+			fmt.Println("custom : Invalid Length")
+			errInvalidLength := &validation.ErrInvalidLengthType{}
+			if errors.As(err, errInvalidLength) {
+				fmt.Println("\t--->", errInvalidLength.ActualLength)
+			}
+
+		}
+		if errors.Is(err, validation.ErrMissingSmallLetter) {
+			fmt.Println("custom : MissingSmallLetter")
+		}
+		if errors.Is(err, validation.ErrMissingCapitalLetter) {
+			fmt.Println("custom : MissingCapitalLetter")
+		}
+		if errors.Is(err, validation.ErrMissingDigit) {
+			fmt.Println("custom : MissingDigit")
+			errMissingDigit := &validation.ErrMissingDigitType{}
+			if errors.As(err, errMissingDigit) {
+				fmt.Println("\t--->", errMissingDigit.Desc)
+			}
+		}
 	}
+	fmt.Println("Done")
 }
--- validation/password.go
+++ validation/password.go
@@ -1,39 +1,123 @@
 package validation
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 	"unicode/utf8"
 )
 
-type PasswordError struct {
-	msg string
+var (
+	ErrInvalidLength        = &ErrInvalidLengthType{}
+	ErrMissingSmallLetter   = errors.New("Password is missing a small letter")
+	ErrMissingCapitalLetter = errors.New("Password is missing a capital letter")
+	ErrMissingDigit         = &ErrMissingDigitType{}
+)
+
+type ErrInvalidLengthType struct {
+	ActualLength int
+	Min          int
+	Max          int
+}
+type ErrMissingDigitType struct {
+	Desc string
 }
 
-func (p *PasswordError) Error() string {
-	return p.msg
+func (e ErrMissingDigitType) Error() string {
+	return "Password is missing a digit"
+}
+
+func (e ErrInvalidLengthType) Error() string {
+	return "Password is invalid length"
+}
+
+type PasswordError struct {
+	msg string
+	err error
+}
+
+func (pe *PasswordError) Error() string {
+	return pe.msg
+}
+func (pe *PasswordError) Is(err error) bool {
+	return pe.err.Error() == err.Error()
+}
+func (pe *PasswordError) Unwrap() error {
+	return pe.err
+}
+func (pe *PasswordError) wrappedBy(e error) {
+	pe.msg = pe.msg + e.Error() + "\n"
+	// e is wrapping pe.
+	pe.err = &Unwrappable{wrapper: e, wrapped: pe.err}
+}
+
+type Unwrappable struct {
+	wrapper error
+	wrapped error
+}
+
+func (e *Unwrappable) As(target interface{}) bool {
+	switch target.(type) {
+	case *ErrInvalidLengthType:
+		if src, ok := e.wrapper.(*ErrInvalidLengthType); ok {
+			v := target.(*ErrInvalidLengthType)
+			v.ActualLength = src.ActualLength
+			v.Max = src.Max
+			v.Min = src.Min
+			return true
+		}
+	case *ErrMissingDigitType:
+		if src, ok := e.wrapper.(*ErrMissingDigitType); ok {
+			v := target.(*ErrMissingDigitType)
+			v.Desc = src.Desc
+			return true
+		}
+	}
+	return false
+
+}
+
+func (e *Unwrappable) Error() string {
+	return e.wrapper.Error()
+}
+
+func (e *Unwrappable) Is(err error) bool {
+	return e.wrapper.Error() == err.Error()
+}
+
+func (e *Unwrappable) Unwrap() error {
+	return e.wrapped
 }
 
 func PasswordValidation(pw string) error {
 	pwError := &PasswordError{}
-	if err := checkLength(pw); err != nil {
-		pwError.msg = err.Error() + "\n"
+	if e := checkLength(pw); e != nil {
+		pwError.wrappedBy(e)
 	}
-
-	if err := containSmallLetter(pw); err != nil {
-		pwError.msg = pwError.msg + err.Error() + "\n"
+	if e := containSmallLetter(pw); e != nil {
+		pwError.wrappedBy(e)
 	}
-
-	if err := containCapitalLetter(pw); err != nil {
-		pwError.msg = pwError.msg + err.Error() + "\n"
+	if e := containCapitalLetter(pw); e != nil {
+		pwError.wrappedBy(e)
 	}
-
-	if err := containDigit(pw); err != nil {
-		pwError.msg = pwError.msg + err.Error() + "\n"
+	if e := containDigit(pw); e != nil {
+		pwError.wrappedBy(e)
 	}
 
 	if pwError.msg != "" {
 		return pwError
+	}
+	return nil
+}
+
+func checkLength(pw string) error {
+	pwLen := utf8.RuneCountInString(pw)
+	if pwLen < 7 || pwLen > 16 {
+		return &ErrInvalidLengthType{
+			ActualLength: pwLen,
+			Min:          7,
+			Max:          16,
+		}
 	}
 	return nil
 }
@@ -48,21 +132,25 @@ func regexHelper(pw, pattern, msg string) error {
 }
 
 func containSmallLetter(pw string) error {
-	return regexHelper(pw, `[a-z]`, "password must contain at least one small letter")
+	err := regexHelper(pw, `[a-z]`, "Password must contain small letter")
+	if err != nil {
+		return ErrMissingSmallLetter
+	}
+	return nil
 }
 
 func containCapitalLetter(pw string) error {
-	return regexHelper(pw, `[A-Z]`, "password must contain at least one capital letter")
+	err := regexHelper(pw, `[A-Z]`, "Password must contain capital letter")
+	if err != nil {
+		return ErrMissingCapitalLetter
+	}
+	return nil
 }
 
 func containDigit(pw string) error {
-	return regexHelper(pw, `[0-9]`, "password must contain at least one digit")
-}
-
-func checkLength(pw string) error {
-	pwLen := utf8.RuneCountInString(pw)
-	if pwLen < 7 || pwLen > 16 {
-		return fmt.Errorf("password length must be between 7 and 16")
+	err := regexHelper(pw, `[0-9]`, "Password must contain digit")
+	if err != nil {
+		return &ErrMissingDigitType{Desc: "Password must contain digit"}
 	}
 	return nil
 }
